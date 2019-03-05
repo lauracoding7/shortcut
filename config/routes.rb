b76c8c600e72319a8ce19e156004fbae306cbecc
Laura Meyer <laura-meyer@live.de>
2019-03-05 11:59:30 +0100

    working on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  # root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
-  resources :users, only: [ :index, :show ] do
-    resources :reviews, only: [ :index, :new, :create ]
-    resources :services, only: [ :index, :new, :create, :destroy ]
+  resources :users, only: [:index, :show] do
+    resources :reviews, only: [:index, :new, :create]
+    resources :services, only: [:index, :new, :create, :destroy]
   end
-  resources :appointments, only: [ :index, :show, :new, :create ] do
-    resources :messages, only: [ :new, :create ]
+  resources :appointments, only: [:index, :show, :new, :create] do
+    resources :messages, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
