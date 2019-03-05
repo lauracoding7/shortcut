7d87cf0fba9d8841c219372bcdc8847691c8eba8
Filippo Tognacci <fitog4@hotmail.it>
2019-03-05 12:58:14 +0100

    fixed some stuff and created pages controller with nothing inside

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   resources :users, only: [:index, :show] do
     resources :reviews, only: [:index, :new, :create]
     resources :services, only: [:index, :new, :create, :destroy]
   end
   resources :appointments, only: [:index, :show, :new, :create] do
-    resources :messages, only: [:new, :create]
+    resources :messages, only: [:index, :new, :create]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
