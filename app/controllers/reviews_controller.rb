750e9ea47460fd11beec9826991e949cc8c6553d
Laura Meyer <laura-meyer@live.de>
2019-03-05 18:34:03 +0100

    reviews controller done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,39 @@
+class ReviewsController < ApplicationController
+  before_action :set_user
+
+  def index
+    if params[:query].present?
+      author = User.where("name ILIKE ?", "%#{params[:query]}%").first
+      @reviews = Review.where(receiver: @user, author: author)
+    else
+      @reviews = Review.where(receiver: @user)
+    end
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    review.author = current_user
+    if @review.save
+       redirect_to user_reviews_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :barber_review)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
+
+
+
