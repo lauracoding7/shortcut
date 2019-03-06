906828ca9eeaa62a1a3f0600ccfd8e21cecc4bc1
Killian Guacide <killian.guacide.bba@edhec.com>
2019-03-06 15:49:13 +0100

    changed the routes and adapted the controller, views needs some work

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,6 @@
 class AppointmentsController < ApplicationController
-  before_action :set_receiver
+  before_action :set_barber, only: [:new, :create]
+  before_action :find_service, only: [:new, :create]
 
   def show
     @appointments = Appointment.find(params[:id])
@@ -7,24 +8,30 @@ class AppointmentsController < ApplicationController
 
   def new
     @appointment = Appointment.new
+
   end
 
   def create
     @appointment_request = Appointment.new(appointment_params)
-    @appointment_request.receiver_id = @user.id
-    @appointment_request.author_id = current_user.id
+    @appointment_request.barber_id = @user.id
+    @appointment_request.client_id = current_user.id
 
-    @appointment_request.save
-
-    redirect_to appointment_path(@appointment)
+    if @appointment_request.save
+      redirect_to appointment_path(@appointment)
+    else
+      render :new
+    end
   end
 
 private
 
-def set_receiver
-  @user = User.find(params[:receiver_id])
+def set_barber
+  @user = User.find(params[:user_id])
+end
+def find_service
+  @service = Service.find(params[:service_id])
 end
 def appointment_params
-  params.require(:appointment).permit(:location_adress, :datetime)
+  params.require(:appointment).permit(:location_address, :datetime)
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  get 'appointments/new'
-  get 'appointments/create'
-  get 'appointments/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   devise_for :users
   resources :users, only: [:index, :show] do
     resources :reviews, only: [:index, :new, :create]
-    resources :services, only: [:index, :new, :create, :destroy]
+    resources :services, only: [:index, :new, :create, :destroy] do
+      resources :appointments, only: [:new, :create]
+    end
   end
-  resources :appointments, only: [:index, :show, :new, :create] do
+  resources :appointments, only: [:index, :show] do
     resources :messages, only: [:index, :new, :create]
   end
 end
