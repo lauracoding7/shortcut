e6a90a776fe9772df30d6ce9b96fc8bf31f24a19
Laura Meyer <laura-meyer@live.de>
2019-03-06 11:10:45 +0100

    some changes made in reviews controller, needs to be updated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,21 +2,24 @@ class ReviewsController < ApplicationController
   before_action :set_user
 
   def index
-    if params[:query].present?
-      author = User.where("name ILIKE ?", "%#{params[:query]}%").first
-      @reviews = Review.where(receiver: @user, author: author)
-    else
-      @reviews = Review.where(receiver: @user)
-    end
+    # if params[:query].present?
+    #   author = User.where("name ILIKE ?", "%#{params[:query]}%").first
+    #   @reviews = Review.where(receiver: @user, author: author)
+    # else
+    @reviews = Review.where(receiver: @user)
+    # end
   end
 
   def new
     @review = Review.new
+    @review.barber_review = # true or false depending on who it writing the review
+    @review.receiver = @user
   end
 
-  def create
+  def create # this needs to be updates
     @review = Review.new(review_params)
     review.author = current_user
+    review.receiver =
     if @review.save
        redirect_to user_reviews_path(@user)
     else
