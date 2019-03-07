b38307c0926e718cf69e74cb44e2023d10b0af7b
Laura Meyer <laura-meyer@live.de>
2019-03-07 11:52:43 +0100

    added dashboard routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   def home
-
+    @user = @User.new
   end
 
+  def dashboard
+  end
+
+  def dashboard_upcoming
+  end
+
+  def dashboard_previous
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get 'dashboard', to: 'pages#dashboard'
+  get 'dashboard', to: 'pages#dashboard', as: 'dashboard'
+  get 'dashboard_upcoming', to: 'pages#dashboard_upcoming', as: 'dashboard_upcoming'
+  get 'dashboard_previous', to: 'pages#dashboard_previous', as: 'dashboard_previous'
   devise_for :users
   resources :users, only: [:index, :show] do
     resources :reviews, only: [:index, :new, :create]
