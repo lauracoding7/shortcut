6610fbc9d2b137e77db80118bca80e1cad5ac383
Filippo Tognacci <fitog4@hotmail.it>
2019-03-10 11:49:04 +0100

    implement button functionality

---
+++ app/javascript/components/filter-buttons.js
@@ -0,0 +1,30 @@
+const initFilterButtons = () => {
+  const buttonCommuters = document.querySelector('#view_commuting_barbers');
+  const buttonHosters = document.querySelector('#view_hosting_barbers');
+
+  if (buttonCommuters && buttonHosters) {
+    const searchResults = document.querySelectorAll('.barber-search-result');
+    const commutingBarbers = document.querySelectorAll('.barber-search-result.commuting-barber');
+    const hostingBarbers = document.querySelectorAll('.barber-search-result.hosting-barber');
+
+    buttonCommuters.addEventListener('click', (event) => {
+      commutingBarbers.forEach((barber) => {
+        if (!barber.classList.contains('hosting-barber')) {
+          barber.classList.toggle('hidden');
+        }
+      })
+      event.currentTarget.classList.toggle('active');
+    })
+
+    buttonHosters.addEventListener('click', (event) => {
+      hostingBarbers.forEach((barber) => {
+        if (!barber.classList.contains('commuting-barber')) {
+          barber.classList.toggle('hidden');
+        }
+      })
+      event.currentTarget.classList.toggle('active');
+    })
+  }
+}
+
+export { initFilterButtons };
